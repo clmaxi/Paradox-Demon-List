b2548f8d731a7a119d06032ecdd833633296b3c7
clmaxi <maximoroblesgonzalez4334@gmail.com>
2025-12-27 16:57:46 -0600

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,8 +24,8 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.4) + 200) *
-        ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
+    let score = (201 / Math.pow(1.15, rank - 1)) *
+    ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
 
